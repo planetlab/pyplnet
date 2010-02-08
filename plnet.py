857a2a0fcebb17e8fd2e4e7a26aebe173b4595af
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2010-02-08 16:29:44 +0000

    can cope with 4.3 ('networks') or 5.0 ('interfaces') + more consistent namings

--- plnet.py
+++ plnet.py
@@ -30,12 +30,16 @@ def InitInterfaces(logger, plc, data, root="", files_only=False, program="NodeMa
     for dev in devs:
         macs[sioc.gifhwaddr(dev).lower()] = dev
 
-    # assume data['networks'] contains this node's NodeNetworks
-    interfaces = {}
-    interface = 1
+    devices_map = {}
+    device_id = 1
     hostname = data.get('hostname',socket.gethostname())
     gateway = None
-    networks = data['networks']
+    # assume data['interfaces'] contains this node's Interfaces
+    # can cope with 4.3 ('networks') or 5.0 ('interfaces')
+    try:
+        interfaces = data['interfaces']
+    except:
+        interfaces = data['networks']
     failedToGetSettings = False
 
     # NOTE: GetInterfaces/NodeNetworks does not necessarily order the interfaces
@@ -52,55 +56,55 @@ def InitInterfaces(logger, plc, data, root="", files_only=False, program="NodeMa
 
     # NOTE: by sorting on 'is_primary' and then reversing (since False is sorted
     # before True) all 'is_primary' interfaces are at the beginning of the list.
-    networks.sort( compare_by('is_primary') )
-    networks.reverse()
+    interfaces.sort( compare_by('is_primary') )
+    interfaces.reverse()
 
-    for network in networks:
-    	logger.verbose('net:InitInterfaces interface %d: %s'%(interface,network))
-    	logger.verbose('net:InitInterfaces macs = %s' % macs)
-        logger.verbose('net:InitInterfaces ips = %s' % ips)
+    for interface in interfaces:
+    	logger.verbose('net:InitInterfaces interface %d: %r'%(device_id,interface))
+    	logger.verbose('net:InitInterfaces macs = %r' % macs)
+        logger.verbose('net:InitInterfaces ips = %r' % ips)
         # Get interface name preferably from MAC address, falling back
         # on IP address.
-        hwaddr=network['mac']
+        hwaddr=interface['mac']
         if hwaddr <> None: hwaddr=hwaddr.lower()
         if hwaddr in macs:
             orig_ifname = macs[hwaddr]
-        elif network['ip'] in ips:
-            orig_ifname = ips[network['ip']]
+        elif interface['ip'] in ips:
+            orig_ifname = ips[interface['ip']]
         else:
             orig_ifname = None
 
 	if orig_ifname:
-       		logger.verbose('net:InitInterfaces orig_ifname = %s' % orig_ifname)
+            logger.verbose('net:InitInterfaces orig_ifname = %s' % orig_ifname)
 	
-        inter = {}
-        inter['ONBOOT']='yes'
-        inter['USERCTL']='no'
-        if network['mac']:
-            inter['HWADDR'] = network['mac']
-        if network['is_primary']:
-            inter['PRIMARY']='yes'
+        details = {}
+        details['ONBOOT']='yes'
+        details['USERCTL']='no'
+        if interface['mac']:
+            details['HWADDR'] = interface['mac']
+        if interface['is_primary']:
+            details['PRIMARY']='yes'
 
-        if network['method'] == "static":
-            inter['BOOTPROTO'] = "static"
-            inter['IPADDR'] = network['ip']
-            inter['NETMASK'] = network['netmask']
-            if network['is_primary']:
-                gateway = network['gateway']
-                if network['dns1']:
-                    inter['DNS1'] = network['dns1']
-                if network['dns2']:
-                    inter['DNS2'] = network['dns2']
+        if interface['method'] == "static":
+            details['BOOTPROTO'] = "static"
+            details['IPADDR'] = interface['ip']
+            details['NETMASK'] = interface['netmask']
+            if interface['is_primary']:
+                gateway = interface['gateway']
+                if interface['dns1']:
+                    details['DNS1'] = interface['dns1']
+                if interface['dns2']:
+                    details['DNS2'] = interface['dns2']
 
-        elif network['method'] == "dhcp":
-            inter['BOOTPROTO'] = "dhcp"
-            inter['PERSISTENT_DHCLIENT'] = "yes"
-            if network['hostname']:
-                inter['DHCP_HOSTNAME'] = network['hostname']
+        elif interface['method'] == "dhcp":
+            details['BOOTPROTO'] = "dhcp"
+            details['PERSISTENT_DHCLIENT'] = "yes"
+            if interface['hostname']:
+                details['DHCP_HOSTNAME'] = interface['hostname']
             else:
-                inter['DHCP_HOSTNAME'] = hostname 
-            if not network['is_primary']:
-                inter['DHCLIENTARGS'] = "-R subnet-mask"
+                details['DHCP_HOSTNAME'] = hostname 
+            if not interface['is_primary']:
+                details['DHCLIENTARGS'] = "-R subnet-mask"
 
         try:
             plc.GetInterfaceTags()
@@ -115,17 +119,17 @@ def InitInterfaces(logger, plc, data, root="", files_only=False, program="NodeMa
             interface_tag_ids = "nodenetwork_setting_ids"
             interface_tag_id = "nodenetwork_setting_id"
 
-        if len(network[interface_tag_ids]) > 0:
+        if len(interface[interface_tag_ids]) > 0:
             try:
                 if version == 4.3:
-                    settings = plc.GetInterfaceTags({interface_tag_id:network[interface_tag_ids]})
+                    settings = plc.GetInterfaceTags({interface_tag_id:interface[interface_tag_ids]})
                 else:
-                    settings = plc.GetNodeNetworkSettings({interface_tag_id:network[interface_tag_ids]})
+                    settings = plc.GetNodeNetworkSettings({interface_tag_id:interface[interface_tag_ids]})
             except:
                 logger.log("net:InitInterfaces FATAL: failed call GetInterfaceTags({'interface_tag_id':{%s})"% \
-                           network['interface_tag_ids'])
+                           interface['interface_tag_ids'])
                 failedToGetSettings = True
-                continue # on to the next network
+                continue # on to the next interface
 
             for setting in settings:
                 # to explicitly set interface name
@@ -135,69 +139,69 @@ def InitInterfaces(logger, plc, data, root="", files_only=False, program="NodeMa
                     
                 settingname = setting[name_key].upper()
                 if settingname in ('IFNAME','ALIAS','CFGOPTIONS','DRIVER'):
-                    inter[settingname]=setting['value']
+                    details[settingname]=setting['value']
                 # wireless settings
                 elif settingname in \
                         [  "MODE", "ESSID", "NW", "FREQ", "CHANNEL", "SENS", "RATE",
                            "KEY", "KEY1", "KEY2", "KEY3", "KEY4", "SECURITYMODE", 
                            "IWCONFIG", "IWPRIV" ] :
-                    inter [settingname] = setting['value']
-                    inter ['TYPE']='Wireless'
+                    details [settingname] = setting['value']
+                    details ['TYPE']='Wireless'
                 else:
                     logger.log("net:InitInterfaces WARNING: ignored setting named %s"%setting[name_key])
 
         # support aliases to interfaces either by name or HWADDR
-        if 'ALIAS' in inter:
-            if 'HWADDR' in inter:
-                hwaddr = inter['HWADDR'].lower()
-                del inter['HWADDR']
+        if 'ALIAS' in details:
+            if 'HWADDR' in details:
+                hwaddr = details['HWADDR'].lower()
+                del details['HWADDR']
                 if hwaddr in macs:
                     hwifname = macs[hwaddr]
-                    if ('IFNAME' in inter) and inter['IFNAME'] <> hwifname:
+                    if ('IFNAME' in details) and details['IFNAME'] <> hwifname:
                         logger.log("net:InitInterfaces WARNING: alias ifname (%s) and hwaddr ifname (%s) do not match"%\
-                                       (inter['IFNAME'],hwifname))
-                        inter['IFNAME'] = hwifname
+                                       (details['IFNAME'],hwifname))
+                        details['IFNAME'] = hwifname
                 else:
                     logger.log('net:InitInterfaces WARNING: mac addr %s for alias not found' %(hwaddr))
 
-            if 'IFNAME' in inter:
+            if 'IFNAME' in details:
                 # stupid RH /etc/sysconfig/network-scripts/ifup-aliases:new_interface()
                 # checks if the "$DEVNUM" only consists of '^[0-9A-Za-z_]*$'. Need to make
                 # our aliases compliant.
-                parts = inter['ALIAS'].split('_')
+                parts = details['ALIAS'].split('_')
                 isValid=True
                 for part in parts:
                     isValid=isValid and part.isalnum()
 
                 if isValid:
-                    interfaces["%s:%s" % (inter['IFNAME'],inter['ALIAS'])] = inter 
+                    devices_map["%s:%s" % (details['IFNAME'],details['ALIAS'])] = details 
                 else:
-                    logger.log("net:InitInterfaces WARNING: interface alias (%s) not a valid string for RH ifup-aliases"% inter['ALIAS'])
+                    logger.log("net:InitInterfaces WARNING: interface alias (%s) not a valid string for RH ifup-aliases"% details['ALIAS'])
             else:
-                logger.log("net:InitInterfaces WARNING: interface alias (%s) not matched to an interface"% inter['ALIAS'])
-            interface -= 1
+                logger.log("net:InitInterfaces WARNING: interface alias (%s) not matched to an interface"% details['ALIAS'])
+            device_id -= 1
         else:
-            if ('IFNAME' not in inter) and not orig_ifname:
-                ifname="eth%d" % (interface-1)
+            if ('IFNAME' not in details) and not orig_ifname:
+                ifname="eth%d" % (device_id-1)
                 # should check if $ifname is an eth already defines
                 if os.path.exists("%s/ifcfg-%s"%(sysconfig,ifname)):
                     logger.log("net:InitInterfaces WARNING: possibly blowing away %s configuration"%ifname)
             else:
-		if ('IFNAME' not in inter) and orig_ifname:
+		if ('IFNAME' not in details) and orig_ifname:
                     ifname = orig_ifname
                 else:
-                    ifname = inter['IFNAME']
-                interface -= 1
-            interfaces[ifname] = inter
+                    ifname = details['IFNAME']
+                device_id -= 1
+            devices_map[ifname] = details
                 
     m = modprobe.Modprobe()
     try:
         m.input("%s/etc/modprobe.conf" % root)
     except:
         pass
-    for (dev, inter) in interfaces.iteritems():
+    for (dev, details) in devices_map.iteritems():
         # get the driver string "moduleName option1=a option2=b"
-        driver=inter.get('DRIVER','')
+        driver=details.get('DRIVER','')
         if driver <> '':
             driver=driver.split()
             kernelmodule=driver[0]
@@ -208,7 +212,7 @@ def InitInterfaces(logger, plc, data, root="", files_only=False, program="NodeMa
     m.output("%s/etc/modprobe.conf" % root, program)
 
     # clean up after any ifcfg-$dev script that's no longer listed as
-    # part of the NodeNetworks associated with this node
+    # part of the Interfaces associated with this node
 
     # list all network-scripts
     files = os.listdir(sysconfig)
@@ -224,7 +228,7 @@ def InitInterfaces(logger, plc, data, root="", files_only=False, program="NodeMa
     if lo in ifcfgs: ifcfgs.remove(lo)
 
     # remove known devices from icfgs list
-    for (dev, inter) in interfaces.iteritems():
+    for (dev, details) in devices_map.iteritems():
         ifcfg = 'ifcfg-'+dev
         if ifcfg in ifcfgs: ifcfgs.remove(ifcfg)
 
@@ -254,22 +258,22 @@ def InitInterfaces(logger, plc, data, root="", files_only=False, program="NodeMa
 
     # Process ifcfg-$dev changes / additions
     newdevs = []
-    for (dev, inter) in interfaces.iteritems():
+    for (dev, details) in devices_map.iteritems():
         (fd, tmpnam) = tempfile.mkstemp(dir=sysconfig)
         f = os.fdopen(fd, "w")
         f.write("# Autogenerated by pyplnet... do not edit!\n")
-        if 'DRIVER' in inter:
-            f.write("# using %s driver for device %s\n" % (inter['DRIVER'],dev))
+        if 'DRIVER' in details:
+            f.write("# using %s driver for device %s\n" % (details['DRIVER'],dev))
         f.write('DEVICE=%s\n' % dev)
         
         # print the configuration values
-        for (key, val) in inter.iteritems():
+        for (key, val) in details.iteritems():
             if key not in ('IFNAME','ALIAS','CFGOPTIONS','DRIVER'):
                 f.write('%s=%s\n' % (key, val))
 
         # print the configuration specific option values (if any)
-        if 'CFGOPTIONS' in inter:
-            cfgoptions = inter['CFGOPTIONS']
+        if 'CFGOPTIONS' in details:
+            cfgoptions = details['CFGOPTIONS']
             f.write('#CFGOPTIONS are %s\n' % cfgoptions)
             for cfgoption in cfgoptions.split():
                 key,val = cfgoption.split('=')
@@ -377,13 +381,13 @@ if __name__ == "__main__":
 
     node = shell.GetNodes({'node_id': [int(args[0])]})
     try:
-        networks = shell.GetInterfaces({'interface_id': node[0]['interface_ids']})
+        interfaces = shell.GetInterfaces({'interface_id': node[0]['interface_ids']})
     except AttributeError:
-        networks = shell.GetNodeNetworks({'nodenetwork_id':node[0]['nodenetwork_ids']})
+        interfaces = shell.GetNodeNetworks({'nodenetwork_id':node[0]['nodenetwork_ids']})
         version = 4.2
 
 
-    data = {'hostname': node[0]['hostname'], 'networks': networks}
+    data = {'hostname': node[0]['hostname'], 'interfaces': interfaces}
     class logger:
         def __init__(self, verbose):
             self.verbosity = verbose
